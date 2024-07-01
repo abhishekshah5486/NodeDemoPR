b64ea9e162921a7921b956d779111eaf97a71899
abhishekshah5486 <abhishek.shah5486@gmail.com>
2024-07-01 09:41:49 +0530

    Create a directory using fs

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const fs = require('fs');
 
-const data = fs.readFileSync('file.txt');
-console.log(data);
-console.log(data.toString());
-console.log(data + "");
+// const data = fs.readFileSync('file.txt');
+// console.log(data);
+// console.log(data.toString());
+// console.log(data + "");
 
-// Removing the old content and putting a new content altogether
-fs.writeFile('file.txt', "Best wishes for your future endeavours.\n", 'utf-8', (err) => {
-    console.log("File modified");
+// // Removing the old content and putting a new content altogether
+// fs.writeFile('file.txt', "Best wishes for your future endeavours.\n", 'utf-8', (err) => {
+//     console.log("File modified");
+// });
+// fs.appendFile('file.txt', 'Greetings, my name is Abhishek. I have completed my PhD from MIT.', 'utf-8', (err) => {
+//     console.log("File has been appended.");
+// })
+
+// Create a directory using fs
+fs.mkdir('dir1', (err) => {
+    if (err) console.log(err);
+    else console.log("dir formed.");
 });
-fs.appendFile('file.txt', 'Greetings, my name is Abhishek. I have completed my PhD from MIT.', 'utf-8', (err) => {
-    console.log("File has been appended.");
-})
+
+// Remove a directory using fs
+
+// Rename file.txt to new-file.txt
