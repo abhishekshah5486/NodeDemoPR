e65b8f8a469cf119b07fb5aed3ea3d5154b8bdc7
abhishekshah5486 <abhishek.shah5486@gmail.com>
2024-07-01 09:43:15 +0530

    Remove a dir using fs

--- index.js
+++ index.js
@@ -14,11 +14,16 @@ const fs = require('fs');
 // })
 
 // Create a directory using fs
-fs.mkdir('dir1', (err) => {
-    if (err) console.log(err);
-    else console.log("dir formed.");
-});
+// fs.mkdir('dir1', (err) => {
+//     if (err) console.log(err);
+//     else console.log("dir formed.");
+// });
 
 // Remove a directory using fs
+fs.rmdir('dir1', (err) => {
+    if (err) console.log(err);
+    else console.log('dir1 removed.')
+})
 
 // Rename file.txt to new-file.txt
+
